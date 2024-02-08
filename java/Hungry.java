a23c05ac7c94df24c750d83a977139b772d631f7
Monikamali0409 <monikamali8263@gmail.com>
2024-02-08 23:18:02 +0530

    java

---
+++ java/Hungry.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i=0; i<t; i++){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    int z = sc.nextInt();
+		    
+		    if((x>=y && x>=z) || (x>=y && x<=z)){
+		        System.out.println("Pizza");
+		    }
+		    else if((x<=y && x>=z) || (x>=y && x<=z)){
+		        System.out.println("Burger");
+		    }
+		    else{
+		        System.out.println("Nothing");
+		    }
+		}
+	}
+}
